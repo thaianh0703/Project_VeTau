47c310ae3b631e5936f981078817957ee12ae7ae
thaianh0703 <54608731+thaianh0703@users.noreply.github.com>
2020-10-21 15:43:36 +0700

    lan 2

---
+++ client/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+export default function App() {
+  	return (
+    	<div>
+			<h1>hello</h1>
+    	</div>
+  	);
+}
+
